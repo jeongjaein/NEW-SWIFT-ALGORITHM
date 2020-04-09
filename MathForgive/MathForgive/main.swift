cbd433d6f6cd4cea6df50819cf1dcf26718c37cc
jeongjaein <wodls96@gmail.com>
2020-04-09 21:08:22 +0900

    MathForgive

---
+++ MathForgive/MathForgive/main.swift
@@ -0,0 +1,91 @@
+//
+//  main.swift
+//  MathForgive
+//
+//  Created by 정재인 on 2020/04/09.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    
+    
+
+    compareAnswers(pattern: firstpattern)
+    compareAnswers(pattern: secondpattern)
+    compareAnswers(pattern: thirdpattern)
+    
+    for i in 0..<3{
+        if score[i] == score.max()!{
+            answer.append(i+1)
+        }
+    }
+    
+    return answer
+}
+
+var score : [Int] = [0,0,0]
+var flag = 0
+var answer : [Int] = []
+let firstpattern = [1,2,3,4,5]
+let secondpattern = [2,1,2,3,2,4,2,5]
+let thirdpattern = [3,3,1,1,2,2,4,4,5,5]
+
+func compareAnswers(pattern : [Int]){
+    for i in answers{
+        if i == pattern[flag]{
+            score[0] += 1
+        }
+        flag += 1
+        if flag == pattern.count {
+            flag = 0
+        }
+    }
+    flag = 0
+}
+
+
+var answers : [Int] = [1,2,3,4,5,1,2,3,4,5,1,2]
+print(solution(answers))
+
+
+
+
+
+
+//    for i in answers{
+//        if i == firstpattern[test]{
+//            score[0] += 1
+//        }
+//        test += 1
+//        if test == 5 {
+//            test = 0
+//        }
+//    }
+//
+//    test = 0
+//
+//    for i in answers{
+//        if i == secondpattern[test]{
+//            score[1] += 1
+//        }
+//        test += 1
+//        if test == 8{
+//            test = 0
+//        }
+//    }
+//
+//    test = 0
+//
+//    for i in answers{
+//        if i == thirdpattern[test]{
+//            score[2] += 1
+//        }
+//        test += 1
+//        if test == 10{
+//            test = 0
+//        }
+//    }
+//
+    
