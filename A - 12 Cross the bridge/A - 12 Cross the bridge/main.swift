60c9815db46dad60fa778c0fa0ba514a24ad99c3
jeongjaein <wodls96@gmail.com>
2020-04-15 22:32:18 +0900

    heap구현x 로직만대충구현

--- A - 12 Cross the bridge/A - 12 Cross the bridge/main.swift
+++ A - 12 Cross the bridge/A - 12 Cross the bridge/main.swift
@@ -13,30 +13,43 @@ import Foundation
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     
     var result = 0//다지나가는데까지 걸린시간
-    var time : [Int] = []
-    var bridge : [Int] = []
-    var flag = 0
-    var remainWeight = 10
+    
+    var time : [Int] = []//시간큐
+    var bridge : [Int] = []//다리에올라와있는거 표현하는 큐
+    
+    var flag = 0//
+    var remainWeight = 10//
     
     
     while true{
         
+        //시간 다지났는지 감시하는 부분
         if let timetest = time.first , bridge_length == result - timetest {
+            
             time.removeFirst()
             bridge.removeFirst()
+            
             remainWeight = weight - (bridge.first ?? 0)
             
         }
+        //다리 남아있는 무게 재서 빈자리있으면 차 올리는 부분
         
         if flag <= truck_weights.count-1{
+            //현재 다리가 올라갈수있는 무게보다 작으면
+            
             if truck_weights[flag] <= remainWeight {
                 bridge.append(truck_weights[flag])//다리에올림
-                remainWeight = weight - (bridge.first ?? 0)
-                time.append(result)
+                
+                remainWeight = weight - (bridge.first ?? 0)//수정
+                
+                time.append(result)//0
+                
                 flag += 1
             }
         }
+        print(bridge)
         result += 1
+        
         if bridge.count == 0{
             break
         }
@@ -44,7 +57,7 @@ func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     return result
 }
 
-var bridge_length = 100
-var weight = 100
-var truck_weights = [10]
+var bridge_length = 2
+var weight = 10
+var truck_weights = [7,4,5,6]
 print(solution(bridge_length,weight,truck_weights))
