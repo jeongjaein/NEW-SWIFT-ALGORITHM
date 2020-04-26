9078086a8fe4131a9cb4cbb629d63282ee2cf997
jeongjaein <wodls96@gmail.com>
2020-04-26 23:52:18 +0900

    fail

---
+++ A - 22 Skill Tree/A - 22 Skill Tree/main.swift
@@ -0,0 +1,83 @@
+//
+//  main.swift
+//  A - 22 Skill Tree
+//
+//  Created by 정재인 on 2020/04/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+//문제 설명
+//선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
+//
+//예를 들어 선행 스킬 순서가 스파크 → 라이트닝 볼트 → 썬더일때, 썬더를 배우려면 먼저 라이트닝 볼트를 배워야 하고, 라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다.
+//
+//위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 스파크 → 힐링 → 라이트닝 볼트 → 썬더와 같은 스킬트리는 가능하지만, 썬더 → 스파크나 라이트닝 볼트 → 스파크 → 힐링 → 썬더와 같은 스킬트리는 불가능합니다.
+//
+//선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+//
+//제한 조건
+//스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
+//스킬 순서와 스킬트리는 문자열로 표기합니다.
+//예를 들어, C → B → D 라면 CBD로 표기합니다
+//선행 스킬 순서 skill의 길이는 1 이상 26 이하이며, 스킬은 중복해 주어지지 않습니다.
+//skill_trees는 길이 1 이상 20 이하인 배열입니다.
+//skill_trees의 원소는 스킬을 나타내는 문자열입니다.
+//skill_trees의 원소는 길이가 2 이상 26 이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
+//입출력 예
+//skill                skill_trees           return
+//"CBD"    ["BACDE", "CBADF", "AECB", "BDA"]    2
+//
+
+import Foundation
+
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+    var stringArray = skill.map {String($0)}
+    
+    var compareArray : [String] = []
+    var possibleSkillTreeCount = 0
+    
+    for i in skill_trees{
+        compareArray = i.map {String($0)}
+        if compareArray.firstIndex(of: stringArray[0]) != -1{
+            
+            for i in 0..<stringArray.count - 1 {
+                if let compare = compareArray.firstIndex(of: stringArray[i]){
+                    
+                    if let  compare2 = compareArray.firstIndex(of: stringArray[i+1]){
+                        if compare > compare2{
+                            break
+                        }
+                        else{
+                            if i == stringArray.count - 2{
+                                print(compareArray,i)
+                                possibleSkillTreeCount += 1
+                            }
+                        }
+                    }
+                    else{
+                        for k in i + 1..<stringArray.count{
+                            if let compare2 = compareArray.firstIndex(of: stringArray[k]){
+                                break
+                            }
+                            else if k == stringArray.count{
+                                possibleSkillTreeCount += 1
+                            }
+                        }
+                        
+                        
+                    }
+                }
+                else{
+                    for k in i + 1..<stringArray.count{
+                        if let compare2 = compareArray.firstIndex(of: stringArray[k]){
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return possibleSkillTreeCount
+}
+
+print(solution("CBD", ["BACDE", "CBADF", "BDA","AECB" ]))
+
