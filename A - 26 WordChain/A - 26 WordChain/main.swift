34246924c3556bb56f2c877352559eb28ad21b8b
jeongjaein <wodls96@gmail.com>
2020-05-15 21:46:40 +0900

    A - 26 Summer Coding : WordChain

---
+++ A - 26 WordChain/A - 26 WordChain/main.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  A - 26 WordChain
+//
+//  Created by 정재인 on 2020/05/15.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ n:Int, _ words:[String]) -> [Int] {
+    var preWord : [String] = []
+    var postWord : [String] = []
+    var finishWord : [String] = []
+    if words[0].count > 0{
+        finishWord.append(words[0])
+    }
+    else{
+        return[1,1]
+    }
+    for i in 0...words.count - 2{
+        if !(finishWord.contains(words[i+1])){
+            if words[i][words[i].index(before: words[i].endIndex)] == words[i+1][words[i+1].startIndex]{
+                if words[i+1].count > 0{
+                }
+                else{
+                    return i > n ? [(i+2) % n,(i+2 / n)+1] : [i+1,1]
+                }
+            }
+            else{
+                return i > n ? [(i+2) % n,(i+2 / n)+1] : [i+1,1]
+            }
+        }
+        else{
+            return i > n ? [(i+2) % n,(i+2 / n)+1] : [i+1,1]
+        }
+    }
+    return [0, 0]
+}
+
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
